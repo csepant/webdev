d2a3aa56e3d78749557f33b481e7aaf0b201a394
csepulveda <csepulveda@stratagemmasys.com>
2018-05-27 02:12:22 -0400

    Worked on jargon.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,11 +16,11 @@
 	<!-- One -->
 		<section id="one" class="wrapper spotlight style1">
 			<div class="inner">
-				<a href="#" class="image"><img src="{{asset('images/pic01.jpg')}}" alt="" /></a>
+				<a href="#" class="image"><img src="{{asset('images/SVG/web-dev.svg')}}" alt="" /></a>
 				<div class="content">
-					<h2 class="major">Magna arcu feugiat</h2>
-					<p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-					<a href="#" class="special">Learn more</a>
+					<h2 class="major">DESARROLLO WEB</h2>
+					<p>Diseñamos páginas web para tu negocio o idea, adaptables a dispositivos móviles, con contenido en inglés y español. Ofrecemos servicios de hosting, administración de contenido y dominio con altos estándares de seguridad y calidad.</p>
+					<a href="#" class="special">VER MÁS</a>
 				</div>
 			</div>
 		</section>
@@ -28,11 +28,11 @@
 	<!-- Two -->
 		<section id="two" class="wrapper alt spotlight style2">
 			<div class="inner">
-				<a href="#" class="image"><img src="{{asset('images/pic02.jpg')}}" alt="" /></a>
+				<a href="#" class="image"><img src="{{asset('images/SVG/ecommerce.svg')}}" alt="" /></a>
 				<div class="content">
-					<h2 class="major">Tempus adipiscing</h2>
-					<p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-					<a href="#" class="special">Learn more</a>
+					<h2 class="major">E-COMMERCE</h2>
+					<p>Te ayudamos a ofrecer tus productos o servicios por internet. Alcanza nuevas audiencias y potencia tus ventas con nuestros servicios de carrito de compra y venta online.</p>
+					<a href="#" class="special">VER MÁS</a>
 				</div>
 			</div>
 		</section>
@@ -40,11 +40,11 @@
 	<!-- Three -->
 		<section id="three" class="wrapper spotlight style3">
 			<div class="inner">
-				<a href="#" class="image"><img src="{{asset('images/pic03.jpg')}}" alt="" /></a>
+				<a href="#" class="image"><img src="{{asset('images/SVG/digital-branding.svg')}}" alt="" /></a>
 				<div class="content">
-					<h2 class="major">Nullam dignissim</h2>
-					<p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-					<a href="#" class="special">Learn more</a>
+					<h2 class="major">BRANDING DIGITAL</h2>
+					<p>Ayudamos a desarrollar tu presencia de marca en el entorno digital con consistencia y coherencia, aprovechando las posibilidades de interacción entre las distintas plataformas y generando mejores experiencias de marca para tus clientes.</p>
+					<a href="#" class="special">VER MÁS</a>
 				</div>
 			</div>
 		</section>
@@ -52,37 +52,39 @@
 	<!-- Four -->
 		<section id="four" class="wrapper alt style1">
 			<div class="inner">
-				<h2 class="major">Vitae phasellus</h2>
-				<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+				<h2 class="major">Valor Agregado</h2>
+				<p>Después de diseñar un sitio web para un cliente, a menudo nos piden que proporcionemos otros servicios (algo relacionados), como administrar campañas de marketing por correo electrónico, crear diversos informes y actualizar tiendas en línea. Esto es perfecto para nosotros porque es compatible con nuestro modelo de negocio. Pero incluso si normalmente solo brindamos servicios de diseño, hay formas en que podemos ofrecer a nuestros clientes aún más valor por trabajar con nostros, y desarrollar una relación a largo plazo que beneficie a todos.</p>
 				<section class="features">
 					<article>
 						<a href="#" class="image"><img src="{{asset('images/pic04.jpg')}}" alt="" /></a>
-						<h3 class="major">Sed feugiat lorem</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-						<a href="#" class="special">Learn more</a>
+						<h3 class="major">1. Informes de Analytics</h3>
+						<p>Diseñamos el sitio con un código de análisis web en su lugar y el uso del sitio está siendo rastreado. Pero le lleva mucho tiempo al cliente revisar los análisis y, lo más importante, interpretar lo que significa. Puede ser beneficioso para nuestros clientes que alguien como nosotros que entiende los datos, cree informes, los interprete y haga sugerencias para mejoras en el sitio basadas en los datos.</p>
+						<!--<a href="#" class="special">Learn more</a>-->
 					</article>
 					<article>
 						<a href="#" class="image"><img src="{{asset('images/pic05.jpg')}}" alt="" /></a>
-						<h3 class="major">Nisl placerat</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-						<a href="#" class="special">Learn more</a>
+						<h3 class="major">2. Monitoreo de Competencia</h3>
+						<p>Independientemente de la industria en la que se encuentre su negocio, hay competidores en línea. Podría valer la pena hacer que alguien vigile los sitios web de los competidores para estar al día con lo que están haciendo, especialmente si puede afectar su negocio.</p>
+						<!--<a href="#" class="special">Learn more</a>-->
 					</article>
 					<article>
 						<a href="#" class="image"><img src="{{asset('images/pic06.jpg')}}" alt="" /></a>
-						<h3 class="major">Ante fermentum</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-						<a href="#" class="special">Learn more</a>
+						<h3 class="major">3. Correo Electrónico</h3>
+						<p>Si envía mensajes de marketing por correo electrónico, puede estar interesado en crear plantillas de correo electrónico que tengan la misma apariencia que el sitio web.</p>
+						<!--<a href="#" class="special">Learn more</a>-->
 					</article>
 					<article>
 						<a href="#" class="image"><img src="{{asset('images/pic07.jpg')}}" alt="" /></a>
-						<h3 class="major">Fusce consequat</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-						<a href="#" class="special">Learn more</a>
+						<h3 class="major">4. Prueba de Usabilidad</h3>
+						<p>Después de que el sitio esté en vivo, o antes de que comience el trabajo en el caso de un rediseño, puede tener sentido obtener información de sus clientes y del público objetivo. Puede gestionar las pruebas de usabilidad creando y compilando encuestas en linea.</p>
+						<!--<a href="#" class="special">Learn more</a>-->
 					</article>
 				</section>
+				<!--
 				<ul class="actions">
 					<li><a href="#" class="button">Browse All</a></li>
 				</ul>
+				-->
 			</div>
 		</section>
 
@@ -91,8 +93,8 @@
 <!-- Footer -->
 <section id="footer">
 	<div id="contact" class="inner">
-		<h2 class="major">Get in touch</h2>
-		<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+		<h2 class="major">CONTACTANOS</h2>
+		<p>Cuentanos tu necesidad. Convertimos problemas en oportunidades. Cotización sin compromisos.</p>
 		@include('incl.messages')
 		{!! Form::open(['action' => 'ContactFormController@store', 'method' => 'POST']) !!}
 			<div class="field">
